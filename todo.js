4521ebbb5b0f5514143f1c8ae011b233b1f96dc6
Sarah <92929752+alaridisarah@users.noreply.github.com>
2023-05-13 19:53:28 +0300

    Adding new task

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
 // Create a "close" button and append it to each list item
 document.getElementById("Addbtn").addEventListener("click", NewTask);
+
 var count= 0;
 // var myNodelist = document.getElementsByTagName("LI");
 // var i;
@@ -12,15 +13,6 @@ var count= 0;
 //     myNodelist[i].appendChild(span);
 // }
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-    }
-}
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
@@ -36,25 +28,12 @@ function NewTask(e) {
     var li = document.createElement("li");
     li.setAttribute("class","list-group-item d-flex align-items-center border-0 mb-2 rounded");
     li.setAttribute("style","background-color: #f4f6f7; padding: 15px; padding-left: 20px ;");
-    var inputValue = document.getElementById("myInput").value;
-    // var labe = document.createTextNode(inputValue);
-    // li.appendChild(labe);
-    // let task = "task"+count;
-    // let inp = document.createElement("input");
-    // inp.setAttribute("type","checkbox");
-    // inp.setAttribute("id",task);
-    // li.appendChild(inp);
 
-    // var labe = document.createElement("label");
-    // labe.setAttribute("for",task);
-    // labe.innerHTML(inputValue);
-    // li.appendChild(inp);
-    
-    // var ic = document.createElement("i");
-    // ic.setAttribute("class","bi bi-trash");
-    // li.appendChild(ic);
-    // count++;
-    li.innerHTML('<input type="checkbox" id="${box-1}"> <label for="box-1">Sustainable typewriter cronut</label> <i class="bi bi-trash" style="color: rgb(167, 7, 7); position: absolute;right: 20px;"></i>');
+    var inputValue = document.getElementById("myInput").value;
+    var task = "task"+count;
+    li.innerHTML = '<input type="checkbox" id="'+task+'"><label for="'+task+'">'+inputValue+'</label><i class="bi bi-trash" style="color: rgb(167, 7, 7); position: absolute;right: 20px;"></i>';
+
+    //ADDing to database
     if (inputValue === '') {
     alert("You must write something!");
     } else {
@@ -62,11 +41,11 @@ function NewTask(e) {
     }
     document.getElementById("myInput").value = "";
 }
-/*   
-<input type="checkbox" id="box-1">
-                        <label for="box-1">Sustainable typewriter cronut</label>
-                        <i class="bi bi-trash" style="color: rgb(167, 7, 7); position: absolute;right: 20px;"></i>
 
-
-
-*/
+function Complete() {
+    // Get the checkbox
+    var checkBox = document.getElementById("box-1");
+    // Get the output text
+    var completedul = document.getElementById("completed");
+    completedul.appendChild(checkBox);
+  }

d88ca9bed14bf7534ee8637347f6ccca0f0544f2
alaridisarah <92929752+alaridisarah@users.noreply.github.com>
2023-05-21 10:33:59 +0300

    fix delete,insert, and complete

--- public/todo.js
+++ public/todo.js
@@ -1,130 +1,94 @@
 // Create a "close" button and append it to each list item
 document.getElementById("Addbtn").addEventListener("click", NewTask);
 
-var count= 0;
+var count = 0;
 
 
 
 // Create a new list item when clicking on the "Add" button
 function NewTask(e) {
-    e.preventDefault();
-    var task = "task"+count;
+  e.preventDefault();
+  var task = "task" + count;
 
-    var inputValue = document.getElementById("myInput").value;
-    // Get a reference to the parent element
-const parentElement = document.getElementById('myList');
+  var inputValue = document.getElementById("myInput").value;
+  // Get a reference to the parent element
+  const parentElement = document.getElementById('myUL');
 
-// Create a new list item element
-const listItemElement = document.createElement('li');
-listItemElement.setAttribute('id', task+"li");
-listItemElement.setAttribute('class', 'list-group-item d-flex align-items-center border-0 mb2 rounded');
-listItemElement.style.backgroundColor = '#f4f6f7';
-listItemElement.style.padding = '15px';
-listItemElement.style.paddingLeft = '20px';
+  // Create a new list item element
+  const listItemElement = document.createElement('li');
+  listItemElement.setAttribute('id', task + "li");
+  listItemElement.setAttribute('class', 'list-group-item d-flex align-items-center border-0 mb2 rounded');
+  listItemElement.style.backgroundColor = '#f4f6f7';
+  listItemElement.style.padding = '15px';
+  listItemElement.style.paddingLeft = '20px';
 
-// Create a new checkbox input element
-const checkboxElement = document.createElement('input');
-checkboxElement.setAttribute('type', 'checkbox');
-checkboxElement.setAttribute('id', task);
-checkboxElement.setAttribute('name', 'checkbox');
-checkboxElement.addEventListener('change', function(event) {
-  Complete(this, event);
-});
+  // Create a new checkbox input element
+  const checkboxElement = document.createElement('input');
+  checkboxElement.setAttribute('type', 'checkbox');
+  checkboxElement.setAttribute('id', task);
+  checkboxElement.setAttribute('name', 'checkbox');
+  checkboxElement.addEventListener('change', function (event) {
+    Complete(this);
+  });
 
-// Create a new label element
-const labelElement = document.createElement('label');
-labelElement.setAttribute('for', task);
-labelElement.textContent = inputValue;
+  // Create a new label element
+  const labelElement = document.createElement('label');
+  labelElement.setAttribute('for', task);
+  labelElement.textContent = inputValue;
 
-// Create a new trash icon element
-const trashIconElement = document.createElement('i');
-trashIconElement.setAttribute('class', 'bi bi-trash');
-trashIconElement.setAttribute('style', 'color: rgb(167, 7, 7); position: absolute; right: 20px;');
-trashIconElement.addEventListener('click', function(event) {
-  RemoveTask(event);
-});
+  // Create a new trash icon element
+  const trashIconElement = document.createElement('i');
+  trashIconElement.setAttribute('class', 'bi bi-trash');
+  trashIconElement.setAttribute('style', 'color: rgb(167, 7, 7); position: absolute; right: 20px;');
+  trashIconElement.addEventListener('click', function (event) {
+    RemoveTask(this);
+  });
 
-// Append the checkbox input element, label element, and trash icon element as children of the list item element
-listItemElement.appendChild(checkboxElement);
-listItemElement.appendChild(labelElement);
-listItemElement.appendChild(trashIconElement);
-
-// Append the list item element as a child of the parent element
-    // var li = document.createElement("li");
-    // li.setAttribute("class","list-group-item d-flex align-items-center border-0 mb-2 rounded");
-    // li.setAttribute("style","background-color: #f4f6f7; padding: 15px; padding-left: 20px ;");
-
-    // var task = "task"+count;
-
-    // //ADDing to database
-    // var inp = document.createElement("input");
-    // inp.setAttribute("type","checkbox");
-    // inp.setAttribute("id",task);
-    // inp.setAttribute("onchange","Complete(this,event)");
-
-    // var labe = document.createElement("label");
-    // labe.setAttribute("for",task);
-    // labe.innerHTML= inputValue;
-
-    // var trash = document.createElement("i");
-    // trash.setAttribute("class","bi bi-trash");
-    // trash.setAttribute("onclick","RemoveTask(event)")
-    // trash.setAttribute("style","color: rgb(167, 7, 7); position: absolute;right: 20px;");
-
-    // li.appendChild(inp);
-    // li.appendChild(labe);
-    // li.appendChild(trash);
+  // Append the checkbox input element, label element, and trash icon element as children of the list item element
+  listItemElement.appendChild(checkboxElement);
+  listItemElement.appendChild(labelElement);
+  listItemElement.appendChild(trashIconElement);
 
 
-
-
-    //li.innerHTML = '<input type="checkbox" id="'+task+'" onchange="Complete(this,event)"><label for="'+task+'">'+inputValue+'</label><i class="bi bi-trash" onclick="RemoveTask(event)" style="color: rgb(167, 7, 7); position: absolute;right: 20px;"></i>';
-
-    if (inputValue === '') {
+  if (inputValue === '') {
     alert("You must write something!");
-    } else {
+  } else {
     document.getElementById("myUL").appendChild(listItemElement);
-    }
-    document.getElementById("myInput").value = "";
+  }
+  document.getElementById("myInput").value = "";
 }
 
-// function Complete(checkboxElem, event) {
-//     //Mark as a completed in DB
 
-//     const item = event.target.parentNode;
-//     const parent = item.getAttribute('id');
-//     if (checkboxElem.checked) {
-//         const completedul = document.getElementById("completed");
-//         completedul.appendChild(document.getElementById(item));
-//       } 
-    
-//   }
 function Complete(checkbox) {
-    if (checkbox.checked) {
-      // Get a reference to the parentunordered list and the target unordered list
-      const parentList = checkbox.parentNode.parentNode;
-      const targetList = document.getElementById('completed');
-      
-      // Clone the list item element and remove the original
-      const listItem = checkbox.parentNode.cloneNode(true);
-      checkbox.parentNode.remove();
-      
-      // Append the cloned list item element to the target list
+  if (checkbox.checked) {
+    // Get a reference to the parentunordered list and the target unordered list
+    const parentList = checkbox.parentNode.parentNode;
+    const targetList = document.getElementById('completed');
 
-      targetList.appendChild(listItem);
-      const checkitem = listItem.querySelector('input[name="checkbox"]')
-      checkitem.checked = true;
-      checkitem.removeAttribute('onchange');
+    // Clone the list item element and remove the original
+    const listItem = checkbox.parentNode.cloneNode(true);
+    checkbox.parentNode.remove();
+
+    // Append the cloned list item element to the target list
+    listItem.querySelector('input[name="checkbox"]').removeAttribute('onchange');
+    listItem.querySelector('input[name="checkbox"]').checked = true;
+    listItem.removeChild(listItem.children[2]);
+    targetList.appendChild(listItem);
 
 
+  }
+}
+
+function RemoveTask(trashButton) {
+    // Get a reference to the parent list item element
+    const listItem = trashButton.closest('li');
+    
+    // Check if the listItem parameter is a valid DOM element
+    if (listItem instanceof Node) {
+      // Get a reference to the parent list element
+      const list = listItem.parentNode;
+    
+      // Remove the list item element
+      list.removeChild(listItem);
     }
   }
-
-function RemoveTask(event){
-    //Delete form DB
-
-    var item = event.target.parentElement.id;
-    var removed = document.getElementById(item);
-    removed.remove();
-
-}

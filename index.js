f0120658b9d64829cacbf6979c36dad5db0f86a0
SarahAlsarami <sarahalsarami09@example.com>
2023-06-03 05:42:31 +0300

    CRUD

--- index.js
+++ index.js
@@ -1,56 +1,108 @@
-
-const express = require("express");
-const mongoose = require("mongoose");
-const port=process.env.port||8080;
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const app = express();
 const url = "mongodb://127.0.0.1:27017/todaydb";
-const bodyParser=require("body-parser")
-var app = express();
-mongoose.connect(url, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+var port = process.env.PORT || 8080;
+const session = require('express-session');
+const passport = require('passport');
+app.use(flash());
+const LocalStrategy = require('passport-local').Strategy;
+app.use(session({
+	secret: 'mysecret',
+	resave: false,
+	saveUninitialized: false
+  }));
+// Connect to MongoDB
+mongoose.connect(url, { useNewUrlParser: true });
+const userSchema = new mongoose.Schema({
+	name: String,
+	email: String,
+	password: String,
+  });
+  
+  const User = mongoose.model('User', userSchema);
+// Use body-parser to parse form data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(passport.initialize());
+app.use(passport.session());
 
-}).then(() => {
-  console.log("connected ok");
-}).catch((e) => {
-  console.log("connected not ok" + e);
-})
-var db= mongoose.connection;
-app.use(bodyParser.json())
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({
-  extended:true
-}))
 
-
-
-
-app.post(
-  "/sign_up",(req,res)=>{
-var name=req.body.name;
-var email=req.body.email;
-var password=req.body.password;
-var data={
-  "name":name,
-  "email":email,
-  "password":password
-}
-db.collection('users').insertOne(data,(err,collection)=>{
-  if (err) throw err;
-      console.log("Record inserted Successfully");
+app.get("/", function (req, res) {
+    res.render("register");
 });
-return res.redirect('Home.html')
-
-}
-
-)
-app.get("/",(req,res)=>{
-res.set({
-  'Access-control-Allow-Origin': '*'
-})
-return  res.redirect('register.html');
-}).listen(port)
-
-console.log('server listing at port :'+port);
+  
 
 
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+
+app.get("/Home", function (req, res) {
+    res.render("Home");
+});
+app.get("/Challenges", function (req, res) {
+    res.render("Challenges");
+});
+app.get("/TODO", function (req, res) {
+    res.render("TODO");
+});
+
+
+app.post('/register', (req, res) => {
+  // Create a new user object with the submitted data
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    pass: req.body.pass
+  });
+
+  // Save the user object to the database
+  user.save()
+    .then(() => {
+		res.render('Home', { user: user });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error registering user');
+    });
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.post('/login', (req, res,) => {
+	
+	User.findOne({ email: req.body.email, password: req.body.password })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).send('<script>alert("the email or password are not correct");</script>');
+      }
+	  res.render('Home', { user: user });
+	  
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error logging in');
+    });
+  });
+
+
+
+app.get("/logout", function (req, res) {
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/login');
+      });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log('Server excute on port :'+port);
+});

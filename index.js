1da892a62589a7f0023315726c2fe24ffc8acfb2
SarahAlsarami <sarahalsarami09@example.com>
2023-06-06 08:27:15 +0300

    db

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-<<<<<<< HEAD
-const fs = require('fs')
-=======
 
-let username;
->>>>>>> c6a5c085e854809bd7e96ae8d414139629b1e51a
+const fs = require('fs')
+
+
 const app = express();
 const url = "mongodb+srv://projrcttoday:123456789010@webproject.ipyi8hd.mongodb.net/";
 var port = process.env.PORT || 8080;
@@ -25,7 +23,7 @@ mongoose.connect(url, { useNewUrlParser: true });
 const Challenge = require('./models/Challenge');
 const User = require('./models/User');
 
-// Use body-parser to parse form data
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
@@ -42,25 +40,12 @@ app.get("/register", function (req, res) {
   res.render("register");
 });
 
-<<<<<<< HEAD
-app.get("/Homev2", function (req, res) {
-  res.render("Homev2");
+app.get("/OldHome", function (req, res) {
+  res.render("OldHome");
 });
 
-app.get("/Challenges", async function (req, res) {
-  try {
-    const users = await User.find({});
-    const userChallenges = users.flatMap((user) => user.challenges);
 
-    res.render("Challenges", { userChallenges: userChallenges });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error retrieving challenges');
-  }
-=======
-app.get("/Home", function (req, res) {
-    res.render("Home",{ user: username });
-});
+
 app.get("/Challenges", function (req, res) {
  res.render("Challenges")
 });
@@ -68,32 +53,12 @@ app.get("/Challenges", function (req, res) {
 app.get("/TODO", function (req, res) {
   res.render("TODO");
 });
-
-app.post("/send-task", (req, res) => {
- 
-  fs.writeFile('public/challenge.txt', req.body.name +"\n" + req.body.date + "\n" + req.body.task +"\n end challenge", (err) => {
-
-<<<<<<< HEAD
-    if (err) throw err;
-
-    else{
-
-       console.log("The file is updated with the given data")
-
-    }
-
- })
-
+app.get("/login", function (req, res) {
+  res.render("login");
 });
-=======
-}); NewChallenge.save().then(() => {
-  res.render("Challange", { NewChallenge: NewChallenge });
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error registering user');
-  });});
->>>>>>> c6a5c085e854809bd7e96ae8d414139629b1e51a
+
+
+
 
 app.post('/register', (req, res) => {
   const user = new User({
@@ -116,7 +81,7 @@ app.post('/register', (req, res) => {
         }
 
      })
-      res.render('Homev2');
+      res.render('OldHome');
     })
     .catch((err) => {
       console.error(err);
@@ -137,7 +102,7 @@ app.post('/login', (req, res,) => {
         return res.status(401).send('<script>alert("the email or password are not correct");</script>');
 
       }
-<<<<<<< HEAD
+
 
       fs.writeFile('public/user.txt', user.name, (err) => {
 
@@ -151,13 +116,13 @@ app.post('/login', (req, res,) => {
 
      })
 
-    res.render('Homev2');
+    res.render('OldHome');
 
    
 
 
 
-    .catch((err) => {
+    }).catch((err) => {
 
       console.error(err);
 
@@ -175,7 +140,7 @@ app.post('/login', (req, res,) => {
       else{
          console.log("The file is updated with the given data"+req.body.singltask)
          const message = 'Aded task succsfully!';
-         res.render('Homev2', { message });
+         res.render('OldHome', { message });
   
       }
   
